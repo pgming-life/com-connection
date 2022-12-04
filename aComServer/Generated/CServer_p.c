7101dedd67f23e8398ef03753db778e64ea83320
pgming-life <96xgsatw5i@gmail.com>
2022-12-04 19:28:56 +0900

    Change project name

--- aComServer/Generated/CServer_p.c
+++ aComServer/Generated/CServer_p.c
@@ -7,8 +7,8 @@
 /* at Tue Jan 19 12:14:07 2038
  */
 /* Compiler settings for CServer.idl:
-    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.01.0622 
-    protocol : all , ms_ext, c_ext, robust
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.01.0622 
+    protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
          __declspec(uuid()), __declspec(selectany), __declspec(novtable)
@@ -16,7 +16,7 @@
 */
 /* @@MIDL_FILE_HEADING(  ) */
 
-#if defined(_M_AMD64)
+#if !defined(_M_IA64) && !defined(_M_AMD64) && !defined(_ARM_)
 
 
 #if _MSC_VER >= 1200
@@ -27,6 +27,9 @@
 #pragma warning( disable: 4232 )  /* dllimport identity*/
 #pragma warning( disable: 4024 )  /* array to pointer mapping*/
 #pragma warning( disable: 4152 )  /* function/data pointer conversion in expression */
+#pragma warning( disable: 4100 ) /* unreferenced arguments in x86 call */
+
+#pragma optimize("", off ) 
 
 #define USE_STUBLESS_PROXY
 
@@ -38,7 +41,6 @@
 
 
 #include "rpcproxy.h"
-#include "ndr64types.h"
 #ifndef __RPCPROXY_H_VERSION__
 #error this stub requires an updated version of <rpcproxy.h>
 #endif /* __RPCPROXY_H_VERSION__ */
@@ -47,7 +49,7 @@
 #include "CServer.h"
 
 #define TYPE_FORMAT_STRING_SIZE   3                                 
-#define PROC_FORMAT_STRING_SIZE   45                                
+#define PROC_FORMAT_STRING_SIZE   43                                
 #define EXPR_FORMAT_STRING_SIZE   1                                 
 #define TRANSMIT_AS_TABLE_SIZE    0            
 #define WIRE_MARSHAL_TABLE_SIZE   0            
@@ -74,10 +76,6 @@ typedef struct _CServer_MIDL_EXPR_FORMAT_STRING
 static const RPC_SYNTAX_IDENTIFIER  _RpcTransferSyntax = 
 {{0x8A885D04,0x1CEB,0x11C9,{0x9F,0xE8,0x08,0x00,0x2B,0x10,0x48,0x60}},{2,0}};
 
-static const RPC_SYNTAX_IDENTIFIER  _NDR64_RpcTransferSyntax = 
-{{0x71710533,0xbeba,0x4937,{0x83,0x19,0xb5,0xdb,0xef,0x9c,0xcc,0x36}},{1,0}};
-
-
 
 extern const CServer_MIDL_TYPE_FORMAT_STRING CServer__MIDL_TypeFormatString;
 extern const CServer_MIDL_PROC_FORMAT_STRING CServer__MIDL_ProcFormatString;
@@ -92,9 +90,16 @@ extern const MIDL_STUBLESS_PROXY_INFO ISumUp_ProxyInfo;
 
 
 
-#if !defined(__RPC_WIN64__)
+#if !defined(__RPC_WIN32__)
 #error  Invalid build platform for this stub.
 #endif
+#if !(TARGET_IS_NT60_OR_LATER)
+#error You need Windows Vista or later to run this stub because it uses these features:
+#error   forced complex structure or array, new range semantics, compiled for Windows Vista.
+#error However, your C/C++ compilation flags indicate you intend to run this app on earlier systems.
+#error This app will fail with the RPC_X_WRONG_STUB_VERSION error.
+#endif
+
 
 static const CServer_MIDL_PROC_FORMAT_STRING CServer__MIDL_ProcFormatString =
     {
@@ -107,37 +112,36 @@ static const CServer_MIDL_PROC_FORMAT_STRING CServer__MIDL_ProcFormatString =
 			0x6c,		/* Old Flags:  object, Oi2 */
 /*  2 */	NdrFcLong( 0x0 ),	/* 0 */
 /*  6 */	NdrFcShort( 0x7 ),	/* 7 */
-/*  8 */	NdrFcShort( 0x20 ),	/* X64 Stack size/offset = 32 */
+/*  8 */	NdrFcShort( 0x10 ),	/* x86 Stack size/offset = 16 */
 /* 10 */	NdrFcShort( 0x10 ),	/* 16 */
 /* 12 */	NdrFcShort( 0x8 ),	/* 8 */
 /* 14 */	0x44,		/* Oi2 Flags:  has return, has ext, */
 			0x3,		/* 3 */
-/* 16 */	0xa,		/* 10 */
+/* 16 */	0x8,		/* 8 */
 			0x41,		/* Ext Flags:  new corr desc, has range on conformance */
 /* 18 */	NdrFcShort( 0x0 ),	/* 0 */
 /* 20 */	NdrFcShort( 0x0 ),	/* 0 */
 /* 22 */	NdrFcShort( 0x0 ),	/* 0 */
-/* 24 */	NdrFcShort( 0x0 ),	/* 0 */
 
 	/* Parameter a */
 
-/* 26 */	NdrFcShort( 0x48 ),	/* Flags:  in, base type, */
-/* 28 */	NdrFcShort( 0x8 ),	/* X64 Stack size/offset = 8 */
-/* 30 */	0x8,		/* FC_LONG */
+/* 24 */	NdrFcShort( 0x48 ),	/* Flags:  in, base type, */
+/* 26 */	NdrFcShort( 0x4 ),	/* x86 Stack size/offset = 4 */
+/* 28 */	0x8,		/* FC_LONG */
 			0x0,		/* 0 */
 
 	/* Parameter b */
 
-/* 32 */	NdrFcShort( 0x48 ),	/* Flags:  in, base type, */
-/* 34 */	NdrFcShort( 0x10 ),	/* X64 Stack size/offset = 16 */
-/* 36 */	0x8,		/* FC_LONG */
+/* 30 */	NdrFcShort( 0x48 ),	/* Flags:  in, base type, */
+/* 32 */	NdrFcShort( 0x8 ),	/* x86 Stack size/offset = 8 */
+/* 34 */	0x8,		/* FC_LONG */
 			0x0,		/* 0 */
 
 	/* Return value */
 
-/* 38 */	NdrFcShort( 0x70 ),	/* Flags:  out, return, base type, */
-/* 40 */	NdrFcShort( 0x18 ),	/* X64 Stack size/offset = 24 */
-/* 42 */	0x8,		/* FC_LONG */
+/* 36 */	NdrFcShort( 0x70 ),	/* Flags:  out, return, base type, */
+/* 38 */	NdrFcShort( 0xc ),	/* x86 Stack size/offset = 12 */
+/* 40 */	0x8,		/* FC_LONG */
 			0x0,		/* 0 */
 
 			0x0
@@ -176,217 +180,14 @@ static const unsigned short ISumUp_FormatStringOffsetTable[] =
     0
     };
 
-
-
-#endif /* defined(_M_AMD64)*/
-
-
-
-/* this ALWAYS GENERATED file contains the proxy stub code */
-
-
- /* File created by MIDL compiler version 8.01.0622 */
-/* at Tue Jan 19 12:14:07 2038
- */
-/* Compiler settings for CServer.idl:
-    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.01.0622 
-    protocol : all , ms_ext, c_ext, robust
-    error checks: allocation ref bounds_check enum stub_data 
-    VC __declspec() decoration level: 
-         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
-         DECLSPEC_UUID(), MIDL_INTERFACE()
-*/
-/* @@MIDL_FILE_HEADING(  ) */
-
-#if defined(_M_AMD64)
-
-
-
-
-#if !defined(__RPC_WIN64__)
-#error  Invalid build platform for this stub.
-#endif
-
-
-#include "ndr64types.h"
-#include "pshpack8.h"
-
-
-typedef 
-NDR64_FORMAT_CHAR
-__midl_frag5_t;
-extern const __midl_frag5_t __midl_frag5;
-
-typedef 
-struct 
-{
-    struct _NDR64_PROC_FORMAT frag1;
-    struct _NDR64_PARAM_FORMAT frag2;
-    struct _NDR64_PARAM_FORMAT frag3;
-    struct _NDR64_PARAM_FORMAT frag4;
-}
-__midl_frag2_t;
-extern const __midl_frag2_t __midl_frag2;
-
-typedef 
-NDR64_FORMAT_UINT32
-__midl_frag1_t;
-extern const __midl_frag1_t __midl_frag1;
-
-static const __midl_frag5_t __midl_frag5 =
-0x5    /* FC64_INT32 */;
-
-static const __midl_frag2_t __midl_frag2 =
-{ 
-/* SumUp */
-    { 
-    /* SumUp */      /* procedure SumUp */
-        (NDR64_UINT32) 524611 /* 0x80143 */,    /* auto handle */ /* IsIntrepreted, [object], HasReturn */
-        (NDR64_UINT32) 32 /* 0x20 */ ,  /* Stack size */
-        (NDR64_UINT32) 16 /* 0x10 */,
-        (NDR64_UINT32) 8 /* 0x8 */,
-        (NDR64_UINT16) 0 /* 0x0 */,
-        (NDR64_UINT16) 0 /* 0x0 */,
-        (NDR64_UINT16) 3 /* 0x3 */,
-        (NDR64_UINT16) 0 /* 0x0 */
-    },
-    { 
-    /* a */      /* parameter a */
-        &__midl_frag5,
-        { 
-        /* a */
-            0,
-            0,
-            0,
-            1,
-            0,
-            0,
-            1,
-            1,
-            0,
-            0,
-            0,
-            0,
-            0,
-            (NDR64_UINT16) 0 /* 0x0 */,
-            0
-        },    /* [in], Basetype, ByValue */
-        (NDR64_UINT16) 0 /* 0x0 */,
-        8 /* 0x8 */,   /* Stack offset */
-    },
-    { 
-    /* b */      /* parameter b */
-        &__midl_frag5,
-        { 
-        /* b */
-            0,
-            0,
-            0,
-            1,
-            0,
-            0,
-            1,
-            1,
-            0,
-            0,
-            0,
-            0,
-            0,
-            (NDR64_UINT16) 0 /* 0x0 */,
-            0
-        },    /* [in], Basetype, ByValue */
-        (NDR64_UINT16) 0 /* 0x0 */,
-        16 /* 0x10 */,   /* Stack offset */
-    },
-    { 
-    /* HRESULT */      /* parameter HRESULT */
-        &__midl_frag5,
-        { 
-        /* HRESULT */
-            0,
-            0,
-            0,
-            0,
-            1,
-            1,
-            1,
-            1,
-            0,
-            0,
-            0,
-            0,
-            0,
-            (NDR64_UINT16) 0 /* 0x0 */,
-            0
-        },    /* [out], IsReturn, Basetype, ByValue */
-        (NDR64_UINT16) 0 /* 0x0 */,
-        24 /* 0x18 */,   /* Stack offset */
-    }
-};
-
-static const __midl_frag1_t __midl_frag1 =
-(NDR64_UINT32) 0 /* 0x0 */;
-
-
-#include "poppack.h"
-
-
-
-/* Object interface: IUnknown, ver. 0.0,
-   GUID={0x00000000,0x0000,0x0000,{0xC0,0x00,0x00,0x00,0x00,0x00,0x00,0x46}} */
-
-
-/* Object interface: IDispatch, ver. 0.0,
-   GUID={0x00020400,0x0000,0x0000,{0xC0,0x00,0x00,0x00,0x00,0x00,0x00,0x46}} */
-
-
-/* Object interface: ISumUp, ver. 0.0,
-   GUID={0x295D9570,0xC579,0x4696,{0x8B,0xE2,0xC0,0x69,0xDE,0xC0,0x6F,0x36}} */
-
-#pragma code_seg(".orpc")
-static const FormatInfoRef ISumUp_Ndr64ProcTable[] =
-    {
-    (FormatInfoRef)(LONG_PTR) -1,
-    (FormatInfoRef)(LONG_PTR) -1,
-    (FormatInfoRef)(LONG_PTR) -1,
-    (FormatInfoRef)(LONG_PTR) -1,
-    &__midl_frag2
-    };
-
-
-static const MIDL_SYNTAX_INFO ISumUp_SyntaxInfo [  2 ] = 
-    {
-    {
-    {{0x8A885D04,0x1CEB,0x11C9,{0x9F,0xE8,0x08,0x00,0x2B,0x10,0x48,0x60}},{2,0}},
-    0,
-    CServer__MIDL_ProcFormatString.Format,
-    &ISumUp_FormatStringOffsetTable[-3],
-    CServer__MIDL_TypeFormatString.Format,
-    0,
-    0,
-    0
-    }
-    ,{
-    {{0x71710533,0xbeba,0x4937,{0x83,0x19,0xb5,0xdb,0xef,0x9c,0xcc,0x36}},{1,0}},
-    0,
-    0 ,
-    (unsigned short *) &ISumUp_Ndr64ProcTable[-3],
-    0,
-    0,
-    0,
-    0
-    }
-    };
-
 static const MIDL_STUBLESS_PROXY_INFO ISumUp_ProxyInfo =
     {
     &Object_StubDesc,
     CServer__MIDL_ProcFormatString.Format,
     &ISumUp_FormatStringOffsetTable[-3],
-    (RPC_SYNTAX_IDENTIFIER*)&_RpcTransferSyntax,
-    2,
-    (MIDL_SYNTAX_INFO*)ISumUp_SyntaxInfo
-    
+    0,
+    0,
+    0
     };
 
 
@@ -395,12 +196,11 @@ static const MIDL_SERVER_INFO ISumUp_ServerInfo =
     &Object_StubDesc,
     0,
     CServer__MIDL_ProcFormatString.Format,
-    (unsigned short *) &ISumUp_FormatStringOffsetTable[-3],
+    &ISumUp_FormatStringOffsetTable[-3],
     0,
-    (RPC_SYNTAX_IDENTIFIER*)&_NDR64_RpcTransferSyntax,
-    2,
-    (MIDL_SYNTAX_INFO*)ISumUp_SyntaxInfo
-    };
+    0,
+    0,
+    0};
 CINTERFACE_PROXY_VTABLE(8) _ISumUpProxyVtbl = 
 {
     &ISumUp_ProxyInfo,
@@ -422,7 +222,7 @@ static const PRPC_STUB_FUNCTION ISumUp_table[] =
     STUB_FORWARDING_FUNCTION,
     STUB_FORWARDING_FUNCTION,
     STUB_FORWARDING_FUNCTION,
-    NdrStubCall3
+    NdrStubCall2
 };
 
 CInterfaceStubVtbl _ISumUpStubVtbl =
@@ -452,7 +252,7 @@ static const MIDL_STUB_DESC Object_StubDesc =
     0,
     0,
     0,  /* notify & notify_flag routine table */
-    0x2000001, /* MIDL flag */
+    0x1, /* MIDL flag */
     0, /* cs routines */
     0,   /* proxy/server info */
     0
@@ -511,10 +311,11 @@ const ExtendedProxyFileInfo CServer_ProxyFileInfo =
     0, /* Filler2 */
     0  /* Filler3 */
 };
+#pragma optimize("", on )
 #if _MSC_VER >= 1200
 #pragma warning(pop)
 #endif
 
 
-#endif /* defined(_M_AMD64)*/
+#endif /* !defined(_M_IA64) && !defined(_M_AMD64) && !defined(_ARM_) */
 
