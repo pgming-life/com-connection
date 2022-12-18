3c7d435b665a08e4bd620643cbb2b8538ae6a4e0
pgming-life <96xgsatw5i@gmail.com>
2022-12-18 22:46:18 +0900

    Revert "Delete EventFiringObject"
    This reverts commit 563008f48fc47e29398f586f104abfe999eb7ba1.

---
+++ dComEventClient/Main.cpp
@@ -0,0 +1,14 @@
+﻿#include "stdafx.h"
+#include "CClient.h"
+
+int main()
+{
+    cout << "▼▼▼▼▼▼  Test Start ▼▼▼▼▼▼" << endl;
+
+    CClient client;
+    client.OnInit();
+    client.Execute();
+
+    cout << "▲▲▲▲▲▲ Test Finish ▲▲▲▲▲▲" << endl;
+    system("PAUSE");
+}
