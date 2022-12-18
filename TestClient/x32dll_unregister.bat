f61eb391c938287bd9792484ab705c37864f9ef0
pgming-life <96xgsatw5i@gmail.com>
2022-12-18 23:23:28 +0900

    Add dll, exe, bat

---
+++ TestClient/x32dll_unregister.bat
@@ -0,0 +1,14 @@
+@echo off
+openfiles > NUL 2>&1 
+if not %ERRORLEVEL% == 0 (
+  rem 一般権限で実行されたため管理者権限で再実行
+  powershell start-process \"%~f0\" -Verb runas
+  goto exit
+)
+
+echo %~dpn0
+cd /d %~dp0
+echo %WINDIR%\SysWOW64\regsvr32 /u EventFiringObject.dll
+%WINDIR%\SysWOW64\regsvr32 /u EventFiringObject.dll
+
+pause
